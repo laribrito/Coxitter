2b684ac1e12adee99f5d1dad7e48459a54cb288c
laribrito <lary.29.ds@gmail.com>
2021-10-19 15:58:44 -0300

    Tela de editar foto adicionada

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 
-from telalogin import Login
-from telacadastro import Cadastro
-
-
 class MyApp(App):
     def build(self):
         sm = ScreenManager()
+        from telalogin import Login
         sm.add_widget(Login(name='login'))
+        from telacadastro import Cadastro
         sm.add_widget(Cadastro(name='cadastro'))
         from telaperfil import Perfil
         sm.add_widget(Perfil(name='perfil'))
-        
+        from telaMudarFoto import MudarFoto
+        sm.add_widget(MudarFoto(name="mudarFoto"))
         
 
         return sm
---
+++ telaMudarFoto.py
@@ -0,0 +1,82 @@
+'''
+Classe da Tela de Abrir Foto.
+
+Esta classe é responsável por carregar a interface de escolha
+para foto do perfil.
+'''
+
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
+from kivy.network.urlrequest import UrlRequest
+from kivy.utils import platform
+import os
+
+# Classe AppConfig
+#from appconfig import AppConfig
+
+# Carrega a interface
+Builder.load_file('telas/mudarFoto.kv')
+
+'''
+Classe TelaAbreFoto
+'''
+class MudarTela(Screen):
+
+    # Elementos de interface
+    lb_msg = ObjectProperty(None)
+    escolha_arquivo = ObjectProperty(None)
+
+    '''
+    Define o diretório padrão do FileChooser de acordo com a plataforma.
+    '''
+    def carregar_pasta(self):
+        if platform == 'linux':
+            self.escolha_arquivo.rootpath = '/home'
+        elif platform == 'android':
+            self.escolha_arquivo.rootpath = '/storage/emulated/0'
+
+    '''
+    Envia uma requisição do perfil via método GET.
+
+    O parâmetro req_headers contém o Bearer token do usuário autenticado.
+    A API do web service deverá retornar o perfil do login informado.
+    '''
+    def carregar_foto(self, pasta, arquivo):
+        try:
+            arq = open(os.path.join(pasta, arquivo[0]), 'rb')
+
+            UrlRequest(f'{AppConfig.servidor}/api/foto/{AppConfig.get_config("login")}',
+                req_headers = {
+                    'Authorization': f'Bearer {AppConfig.get_config("token")}',
+                },
+                req_body = arq.read(),
+                on_success = self.foto_sucesso,
+                on_error = self.erro,
+            )
+
+            arq.close()
+        except IndexError:
+            self.lb_msg.text = 'Você precisa selecionar um arquivo.'
+
+    '''
+    Recebe a resposta do envio da foto.
+
+    Em caso de sucesso, retorna para a tela de perfil.
+    Em caso de erro, exibe uma mensagem de erro.
+    '''
+    def foto_sucesso(self, req, resposta):
+        if (resposta['status'] == 0):
+            # Transiciona para a tela de perfil
+            self.manager.transition.direction = 'right'
+            self.manager.current = 'perfil'
+            self.manager.current_screen.carregar_perfil(AppConfig.get_config('login'))
+        else:
+            # Exibe a mensagem de erro na resposta
+            self.lb_msg.text = resposta['msg']
+
+    '''
+    Efetua o tratamento em caso de erro ao efetuar a requisição.
+    '''
+    def erro(self, req, erro):
+        self.lb_msg = 'Erro.'
